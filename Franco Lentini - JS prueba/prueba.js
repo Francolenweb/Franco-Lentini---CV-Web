3b24ec53e43d9101127e5cfc104a8c035447fcd9
Francolenweb <89050330+Francolenweb@users.noreply.github.com>
2021-09-21 13:31:56 -0300

    Add files via upload

---
+++ Franco Lentini - JS prueba/prueba.js
@@ -0,0 +1,15 @@
+var dios = prompt("Quien es el más grande de todos? Pista: Fue jugador de fútbol.")
+
+
+
+if ((dios == "Maradona") || (dios == "maradona") || (dios == "diego maradona") || (dios == "Diego Maradona") || (dios == "Diego") || (dios == "El Diego")){
+    alert("Muy bien! El más grande de todos fue y será " + dios + "!")
+}
+
+else{
+    alert("No! " + dios + " no es el más grande. El Diego es la única respuesta correcta!" )
+}
+
+
+
+

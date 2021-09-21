95611484591a77ed8ec4ac129e94dd705b7285f2
Francolenweb <89050330+Francolenweb@users.noreply.github.com>
2021-09-21 13:36:46 -0300

    Commit JS

--- Franco Lentini - JS prueba/prueba.js
+++ Franco Lentini - JS prueba/prueba.js
@@ -2,13 +2,17 @@ var dios = prompt("Quien es el más grande de todos? Pista: Fue jugador de fútb
 
 
 
-if ((dios == "Maradona") || (dios == "maradona") || (dios == "diego maradona") || (dios == "Diego Maradona") || (dios == "Diego") || (dios == "El Diego")){
-    alert("Muy bien! El más grande de todos fue y será " + dios + "!")
+if ((dios == "Maradona") || (dios == "maradona") || (dios == "diego maradona") || (dios == "Diego Maradona")){
+    alert("Muy bien! El más grande de todos fue y será " + dios + "!");
+}
+else if ((dios == "Diego") || (dios == "El Diego")){
+    alert("Muy bien! El más grande de todos fue y será " + dios + "!");
+}
+else{
+    alert("No! " + dios + " no es el más grande. El Diego es la única respuesta correcta!" );
 }
 
-else{
-    alert("No! " + dios + " no es el más grande. El Diego es la única respuesta correcta!" )
-}
+
 
 
 

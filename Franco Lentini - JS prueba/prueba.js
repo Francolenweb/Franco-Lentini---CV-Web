855c8d5087857fb135be089530e455973944cb49
Francolenweb <89050330+Francolenweb@users.noreply.github.com>
2021-09-27 17:14:26 -0300

    Update prueba.js

--- Franco Lentini - JS prueba/prueba.js
+++ Franco Lentini - JS prueba/prueba.js
@@ -1,9 +1,12 @@
 let votoUsuario;
 let votosB = 0;
 let votosR = 0;
+let votoVacio = 0;
+let muestra = 0;
 
 do {
   votoUsuario = prompt("Que equipo es más grande: Boca(B) O River(R)");
+  muestra = votosB + votosR + votoVacio
 
   if (votoUsuario == "B" || votoUsuario == "Boca") {
     alert("VOTO Boca");
@@ -17,28 +20,36 @@ do {
   
   else {
     alert("VOTO EN BLANCO");
+    votoVacio = votoVacio + 1
   }
 
-} while (votosB < 5 && votosR < 5);
+} while (muestra <=10);
+
+
 
 if (votosB < 5 || votosR < 5) {
   
   if (votosB > votosR) {
     console.log("VOTOS Boca: " + votosB);
     console.log("VOTOS River: " + votosR);
-    console.log("GANO Boca ");
+    console.log("Votos en Blanco: " + votoVacio);
+    console.log("GANO Boca. " + "Cantidad de votos: " + muestra);
+    
   } 
   
   else if (votosR > votosB) {
     console.log("VOTOS Boca: " + votosB);
     console.log("VOTOS River: " + votosR);
-    console.log("GANO River ");
+    console.log("Votos en Blanco: " + votoVacio);
+    console.log("GANO River. " + "Cantidad de votos: " + muestra);
+   
   } 
   
   else {
     console.log("VOTOS Boca: " + votosB);
     console.log("VOTOS River: " + votosR);
-    console.log("EMPATE");
+    console.log("Votos en Blanco: " + votoVacio);
+    console.log("Empate. " + "Cantidad de votos: " + muestra);
+    
   }
-  
 }

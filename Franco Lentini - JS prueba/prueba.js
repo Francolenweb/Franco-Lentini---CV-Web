760e6b111ab2a6cea23cd466773d3368af206f16
Francolenweb <89050330+Francolenweb@users.noreply.github.com>
2021-09-27 11:17:18 -0300

    Update prueba.js

--- Franco Lentini - JS prueba/prueba.js
+++ Franco Lentini - JS prueba/prueba.js
@@ -1,45 +1,44 @@
-do{
-    let votosB = parseInt(0);
-    let votosR = parseInt(0);
+let votoUsuario;
+let votosB = 0;
+let votosR = 0;
 
-}while(( votosB == 5) || (votosR == 5)){
+do {
+  votoUsuario = prompt("Que equipo es más grande: Boca(B) O River(R)");
 
-    let votoUsuario = prompt("Que equipo es más grande: Boca(B) O River(R)");
-    
-    if(( votoUsuario == "B") || (votoUsuario == "Boca")){
+  if (votoUsuario == "B" || votoUsuario == "Boca") {
+    alert("VOTO Boca");
+    votosB = votosB + 1;
+  } 
+  
+  else if (votoUsuario == "R" || votoUsuario == "River") {
+    alert("VOTO River");
+    votosR = votosR + 1;
+  } 
+  
+  else {
+    alert("VOTO EN BLANCO");
+  }
 
-        alert("VOTO Boca");
-        votosB = votosB + 1;
-    }
+} while (votosB < 5 && votosR < 5);
 
-    else if(( votoUsuario == "R") || (votoUsuario == "River")){​
-        alert("VOTO River");
-        votosR = votosR + 1;
-
-    }
-    else{
-        alert("VOTO EN BLANCO");
-    }
+if (votosB < 5 || votosR < 5) {
+  
+  if (votosB > votosR) {
+    console.log("VOTOS Boca: " + votosB);
+    console.log("VOTOS River: " + votosR);
+    console.log("GANO Boca ");
+  } 
+  
+  else if (votosR > votosB) {
+    console.log("VOTOS Boca: " + votosB);
+    console.log("VOTOS River: " + votosR);
+    console.log("GANO River ");
+  } 
+  
+  else {
+    console.log("VOTOS Boca: " + votosB);
+    console.log("VOTOS River: " + votosR);
+    console.log("EMPATE");
+  }
+  
 }
-
-if( votosB > votosR){
-    
-        console.log("VOTOS Boca: " + votosB);
-        console.log("VOTOS River: " + votosR);
-        console.log("GANO Boca ");
-    }
-    
-    else if( votosR > votosB){
-    
-        console.log("VOTOS Boca: " + votosB);
-        console.log("VOTOS River: " + votosR);
-        console.log("GANO River ");
-   
-   
-    }
-    else{
-    
-        console.log("VOTOS Boca: " + votosB);
-        console.log("VOTOS River: " + votosR);
-        console.log("EMPATE");
-    }

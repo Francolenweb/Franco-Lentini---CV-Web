4f2bae3a294d2a068851b46cfd109724262913dd
Francolenweb <89050330+Francolenweb@users.noreply.github.com>
2021-10-05 10:27:31 -0300

    Update prueba.js

--- Franco Lentini - JS prueba/prueba.js
+++ Franco Lentini - JS prueba/prueba.js
@@ -1,55 +1,29 @@
-let votoUsuario;
-let votosB = 0;
-let votosR = 0;
-let votoVacio = 0;
-let muestra = 0;
+let promedioFinal = 0;
+let suma = 0;
+let mayores = 0;
+let menores = 0;
 
-do {
-  votoUsuario = prompt("Que equipo es más grande: Boca(B) O River(R)");
-  muestra = votosB + votosR + votoVacio
-
-  if (votoUsuario == "B" || votoUsuario == "Boca") {
-    alert("VOTO Boca");
-    votosB = votosB + 1;
-  } 
-  
-  else if (votoUsuario == "R" || votoUsuario == "River") {
-    alert("VOTO River");
-    votosR = votosR + 1;
-  } 
-  
-  else {
-    alert("VOTO EN BLANCO");
-    votoVacio = votoVacio + 1
-  }
-
-} while (muestra <=10);
+function promedio(){
+    for (let i = 1; i <=5; i++){
+        let edad = parseInt(prompt("Muestra: Ingrese Edad del candidato"));
+        suma = suma + edad
 
 
-
-if (votosB < 5 || votosR < 5) {
-  
-  if (votosB > votosR) {
-    console.log("VOTOS Boca: " + votosB);
-    console.log("VOTOS River: " + votosR);
-    console.log("Votos en Blanco: " + votoVacio);
-    console.log("GANO Boca. " + "Cantidad de votos: " + muestra);
+        if(edad >=18){
+            mayores = mayores + 1
+           }
     
-  } 
-  
-  else if (votosR > votosB) {
-    console.log("VOTOS Boca: " + votosB);
-    console.log("VOTOS River: " + votosR);
-    console.log("Votos en Blanco: " + votoVacio);
-    console.log("GANO River. " + "Cantidad de votos: " + muestra);
-   
-  } 
-  
-  else {
-    console.log("VOTOS Boca: " + votosB);
-    console.log("VOTOS River: " + votosR);
-    console.log("Votos en Blanco: " + votoVacio);
-    console.log("Empate. " + "Cantidad de votos: " + muestra);
-    
-  }
-}
+           else{
+               menores = menores + 1
+           }        
+       }
+
+
+       
+
+       promedioFinal = suma / 5
+       console.log(promedioFinal)
+       return promedioFinal
+ }
+
+ alert("La edad promedio de los candidatos ingresados son de: " + promedio() + ". Donde contamos con " + mayores + " mayores y " + menores + " menores.");

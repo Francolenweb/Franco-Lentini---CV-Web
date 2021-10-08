858157bb9c074f75ed6419a7cc56eb97e64fc401
Francolenweb <89050330+Francolenweb@users.noreply.github.com>
2021-10-08 11:31:48 -0300

    Update prueba.js

--- Franco Lentini - JS prueba/prueba.js
+++ Franco Lentini - JS prueba/prueba.js
@@ -1,29 +1,33 @@
-let promedioFinal = 0;
-let suma = 0;
-let mayores = 0;
-let menores = 0;
 
-function promedio(){
-    for (let i = 1; i <=5; i++){
-        let edad = parseInt(prompt("Muestra: Ingrese Edad del candidato"));
-        suma = suma + edad
+class autos{
+    constructor( modelo , ID , precio, stock){
 
-
-        if(edad >=18){
-            mayores = mayores + 1
-           }
-    
-           else{
-               menores = menores + 1
-           }        
-       }
+        this.modelo = modelo;
+        this.ID = ID;
+        this.precio = precio;
+        this.stock = stock;
 
 
-       
+    }
 
-       promedioFinal = suma / 5
-       console.log(promedioFinal)
-       return promedioFinal
- }
+}
 
- alert("La edad promedio de los candidatos ingresados son de: " + promedio() + ". Donde contamos con " + mayores + " mayores y " + menores + " menores.");
+
+
+let modelo = [ "Agile" ,"Fiesta" , "TT" ];
+let ID = [ 000 , 001 , 002 ];
+let precio =[750000, 1200000 , 5000000  ];
+let stock = [ 15 , 10 , 5 ];
+
+modelo.push("BMW");
+ID.push(003);
+precio.push(3000000);
+stock.push(3);
+
+
+
+
+
+    console.log("El modelo de auto: " +  modelo[3])
+    console.log("Esta disponible a solamente un precio de $" +  precio[3] + "!");
+    console.log("No desaproveches esta oferta! Solamente quedan: " +  stock[3] + " autos.")

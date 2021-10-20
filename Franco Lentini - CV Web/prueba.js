d624409ace07448efd80083e1cb5c8958b90d1b9
Francolenweb <89050330+Francolenweb@users.noreply.github.com>
2021-10-19 22:23:04 -0300

    Update prueba.js

--- Franco Lentini - CV Web/prueba.js
+++ Franco Lentini - CV Web/prueba.js
@@ -1,7 +1,7 @@
 class Usuario{
-    constructor( nombre ,company, email, telefono, roleName, diasContrato ){
+    constructor( company, email, telefono, roleName, diasContrato ){
 
-        this.nombre = nombre
+    
         this.company = company;
         this.email = email;
         this.telefono = telefono;
@@ -14,12 +14,21 @@ class Usuario{
 }
 
 
+    for( let i = 0 ; i < nuevosDatos.length ; i++){
 
-function nuevosDatos(usuario1) {
+        let usuarioNuevo = [ usuarioNuevo.value]
+
+        console.log(usuarioNuevo)
+    }
 
     
 
-    var usuario1 = new Usuario( nombre = prompt("Ingrese su nombre"),company = prompt("Ingrese su compañía"), email = prompt("Ingrese su email"), telefono = prompt("Ingrese su numero de telefono"), roleName = prompt("Ingrese el nombre del puesto"), diasContrato = parseInt(prompt("Ingrese los dias que se necesita cubrir el rol")));
+
+function nuevosDatos() {
+
+    
+
+    let usuario1 = new Usuario( company = prompt("Ingrese su compañía"), email = prompt("Ingrese su email"), telefono = prompt("Ingrese su numero de telefono"), roleName = prompt("Ingrese el nombre del puesto"), diasContrato = parseInt(prompt("Ingrese los dias que se necesita cubrir el rol")));
 
 
 
@@ -28,13 +37,13 @@ function nuevosDatos(usuario1) {
     
     
     if ((this.company == "Accenture") ||  (this.company == "accenture")) {
-        alert("Hola " + this.nombre +", actualmente trabajo para Accenture y estoy interesado de cambiar de empresa. De todas maneras, agradezco el contacto. Gracias!");
+        alert("Gracias por la oferta! Sin embargo, actualmente trabajo para Accenture y estoy interesado de cambiar de empresa. De todas maneras, agradezco el contacto. Gracias!");
     }
     else if (this.company == "") {
         alert("No he podido registrar tu compañia. Por favor vuelva a ingresar sus datos. Gracias");
     } 
     else{
-        alert("Gracias " + this.nombre + "!. Estaré procesando la oferta y te contactare a " + this.email + " o a este telefono "+ this.telefono + " en caso de que la oferta me interese. Gracias por tu tiempo!");
+        alert("Gracias!. Estaré procesando la oferta y te contactare a " + this.email + " o a este telefono "+ this.telefono + " en caso de que la oferta me interese. Gracias por tu tiempo!");
     };
 
 
@@ -53,6 +62,48 @@ function nuevosDatos(usuario1) {
 }
 
 
+function validar(){
 
-console.log(nuevosDatos());
+    let nombreUsuario = document.getElementById("nombreUsuario");
+    
 
+   
+    if( nombreUsuario.value != "" ){
+        let parrafo = document.createElement("p");
+        parrafo.innerHTML = "Bienvenido/a, " + nombreUsuario.value + "!";
+
+        parrafo.style.color = "white";
+        parrafo.style.backgroundColor = "black";
+        parrafo.style.fontSize = "30px";
+        parrafo.style. marginTop = "10px"
+        parrafo.style.fontFamily = "Trebuchet MS";
+        parrafo.style.fontWeight = "700px"
+        parrafo.style.textAlign ="center";
+        console.log( parrafo);
+        let mensaje = document.getElementById("mensaje");
+        mensaje.appendChild( parrafo );
+        
+
+
+
+    }
+    else{
+        let parrafo = document.createElement("p");
+        parrafo.innerHTML = "No pudimos registrar su nombre. Vuelva a ingresar su nombre";
+
+        parrafo.style.color = "white";
+        parrafo.style.backgroundColor = "black";
+        parrafo.style.fontSize = "30px";
+        parrafo.style. marginTop = "10px"
+        parrafo.style.fontFamily = "Trebuchet MS";
+        parrafo.style.fontWeight = "700px"
+        parrafo.style.textAlign ="center";
+        console.log( parrafo);
+        let mensaje = document.getElementById("mensaje");
+        mensaje.appendChild( parrafo );
+    }
+
+
+   
+
+}
